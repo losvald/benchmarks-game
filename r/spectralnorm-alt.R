0440e47890a56b59ef0ea433fddf2a30104ebe0c
Leo Osvald <leo.osvald@gmail.com>
2011-10-06 13:10:53 -0400

    Added source codes from shootout for C, Java, Python and Ruby. The fastest non-MT implementation was chosen or if such did not exist, the MT was eliminated (mostly Python). Renamed some spectralnorm R sources to match the same naming conventions.

--- r/spectralnorm-alt.R
+++ r/spectralnorm-alt.R
@@ -5,28 +5,17 @@
 # Contributed by Leo Osvald
 # ------------------------------------------------------------------
 
-spectralnorm_naive <- function(args) {
+spectralnorm <- function(args) {
     n = ifelse(length(args), as.integer(args[[1]]), 100L)
     options(digits=10)
 
-    eval_A <- function(i, j) 1 / ((i + j) * (i + j + 1) / 2 + i + 1)
-    eval_A_times_u <- function(u) {
-        ret <- rep(0, n)
-        for (i in 1:n)
-	    for (j in 0:n1)
-                ret[[i]] <- ret[[i]] + u[[j + 1]] * eval_A(i - 1, j)
-        return(ret)
-    }
-    eval_At_times_u <- function(u) {
-        ret <- rep(0, n)
-        for (i in 1:n)
-	    for (j in 0:n1)
-                ret[[i]] <- ret[[i]] + u[[j + 1]] * eval_A(j, i - 1)
-        return(ret)
-    }
+    eval_A <- function(i, j) 1 / ((i + j - 2) * (i + j - 1) / 2 + i)
+    eval_A_times_u <- function(u) u %*% g_eval_A_mat
+    eval_At_times_u <- function(u) u %*% g_eval_At_mat
     eval_AtA_times_u <- function(u) eval_At_times_u(eval_A_times_u(u))
 
-    n1 <- n - 1
+    g_eval_A_mat <- outer(seq(n), seq(n), FUN=eval_A)
+    g_eval_At_mat <- t(g_eval_A_mat)
     u <- rep(1, n)
     v <- rep(0, n)
     for (itr in seq(10)) {
@@ -38,4 +27,4 @@ spectralnorm_naive <- function(args) {
 }
 
 if (!exists("i_am_wrapper"))
-    spectralnorm_naive(commandArgs(trailingOnly=TRUE))
+    spectralnorm(commandArgs(trailingOnly=TRUE))

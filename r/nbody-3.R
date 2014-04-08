658148d3cc6ed84eee658ebd417de698f6ac1216
Leo Osvald <losvald@purdue.edu>
2014-04-08 16:39:21 -0400

    Add GPLv2 licence where applicable (i.e. not to override other licences).

--- r/nbody-3.R
+++ r/nbody-3.R
@@ -5,6 +5,25 @@
 # Contributed by Leo Osvald
 # ------------------------------------------------------------------
 
+# nbody-3.R
+#
+# Copyright (C) 2011 Leo Osvald (losvald@purdue.edu)
+#
+# This file is part of R Shootout.
+#
+# R Shootout is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# R Shootout is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with R Shootout. If not, see <http://www.gnu.org/licenses/>.
+
 pi <- 3.141592653589793
 solar_mass <- 4 * pi * pi
 days_per_year <- 365.24
--- scripts/compile-r.sh
+++ scripts/compile-r.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# compile-r.sh
+#
+# Copyright (C) 2011 Leo Osvald (losvald@purdue.edu)
+#
+# This file is part of R Shootout.
+#
+# R Shootout is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# R Shootout is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with R Shootout. If not, see <http://www.gnu.org/licenses/>.
+
 source ~losvald/scripts/bash-common.sh
 
 if [[ $# -lt 2 ]]; then

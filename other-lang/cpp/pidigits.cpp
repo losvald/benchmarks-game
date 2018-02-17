97a701c1c0be8ec98f096c0fa6fd2a23d7f77cd0
Leo Osvald <leo.osvald@gmail.com>
2018-02-17 15:08:08 -0500

    Add explicit scope for each immutable binding

--- other-lang/cpp/pidigits.cpp
+++ other-lang/cpp/pidigits.cpp
@@ -407,36 +407,55 @@ void PiDigits(int limit) {
                   const BigInt& a0) -> void {
     if (i0 < limit) {
       ++k;
-      const auto& t0 = n_div_k * kTwo;
-      const auto& n = n_div_k * BigInt(k);
-      const BigInt k2(2*k + 1);
-      const auto& a = (a0 + t0) * k2;
-      const auto& d = d0 * k2;
-      if (a >= n) {
-        const auto& three_n_plus_a = n * kThree + a;
-        const auto& t = three_n_plus_a / d;
-        // u = three_n_plus_a % d + n;
-        const auto& td = t * d;
-        const auto& u = three_n_plus_a - td + n;
-        if (d > u) {
-          ns = ns * 10 + (int)t;
-          int i = i0 + 1;
-          if (i % 5 == 0) {
-            std::cout.fill('0');
-            std::cout.width(5);
-            std::cout << ns;
-            if (i % 2 == 0)
-              std::cout << "\t:" << i << std::endl;
-            ns = 0;
+      { const auto& t0 = n_div_k * kTwo;
+        { const auto& n = n_div_k * BigInt(k);
+          { const BigInt k2(2*k + 1);
+            { const auto& a0t0sum = a0 + t0;
+              { const auto& a = a0t0sum * k2;
+                { const auto& d = d0 * k2;
+                  if (a >= n) {
+                    { const auto& nMul3 = n * kThree;
+                      { const auto& three_n_plus_a = nMul3 + a;
+                        { const auto& t = three_n_plus_a / d;
+                          // u = three_n_plus_a % d + n;
+                          { const auto& td = t * d;
+                            { const auto& u_n = three_n_plus_a - td;
+                              { const auto& u = u_n + n;
+                                if (d > u) {
+                                  ns = ns * 10 + (int)t;
+                                  int i = i0 + 1;
+                                  if (i % 5 == 0) {
+                                    std::cout.fill('0');
+                                    std::cout.width(5);
+                                    std::cout << ns;
+                                    if (i % 2 == 0)
+                                      std::cout << "\t:" << i << std::endl;
+                                    ns = 0;
+                                  }
+                                  { const auto& a_td = a - td;
+                                    { const auto& aNext = a_td * kTen;
+                                      { const auto& n10 = n * kTen;
+                                        rec(rec, i, n10, d, aNext);
+                                      }
+                                    }
+                                  }
+                                } else {
+                                  rec(rec, i0, n, d, a);
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  } else
+                    rec(rec, i0, n, d, a);
+                }
+              }
+            }
           }
-          const auto& aNext = (a - td) * kTen;
-          const auto& n10 = n * kTen;
-          rec(rec, i, n10, d, aNext);
-        } else {
-          rec(rec, i0, n, d, a);
         }
-      } else
-        rec(rec, i0, n, d, a);
+      }
     }
   };
   rec0(rec0, 0, kOne, kOne, kZero);

06abc9f8ddfc1927fecf69b483a347def1fa77f9
Leo Osvald <leo.osvald@gmail.com>
2018-02-26 13:55:43 -0500

    Use a subset of Scala with explicit body boundaries in fun literals

--- other-lang/miniscala/src/lib/big.scala
+++ other-lang/miniscala/src/lib/big.scala
@@ -63,7 +63,7 @@ private[big] abstract class BaselessArrayInt extends Big {
     f(ret)
   }
   def toInt(n: I): Int = {
-    withSgn(n, { (sgn: Int) =>
+    withSgn(n, (sgn: Int) => {
       var ret = 0
       var i = 0
       while (i < n.length) {
@@ -75,18 +75,18 @@ private[big] abstract class BaselessArrayInt extends Big {
   }
 
   def withAdd[U](lhs: I, rhs: I, f: I => U): U = {
-    withSgn(lhs, { (lhsSgn: Int) =>
-      withSgn(rhs, { (rhsSgn: Int) =>
+    withSgn(lhs, (lhsSgn: Int) => {
+      withSgn(rhs, (rhsSgn: Int) => {
         if (rhsSgn == 0) f(lhs)
         else if (lhsSgn == 0) f(rhs)
         else if (lhsSgn == rhsSgn) {
-          withAddMag(lhs, rhs, { (sum: I) =>
+          withAddMag(lhs, rhs, (sum: I) => {
             sgnSet(sum, lhsSgn)
             f(sum)
           })
         }
         else {
-          withMagDiff(lhs, rhs, { (sum: I) =>
+          withMagDiff(lhs, rhs, (sum: I) => {
             sgnSet(sum, sgnProd(lhsSgn, cmp(lhs, rhs)))
             f(sum)
           })
@@ -100,13 +100,13 @@ private[big] abstract class BaselessArrayInt extends Big {
     if (sgn(rhs) == 0) f(lhs)
     else if (sgn(lhs) == 0) f(rhs)
     else {
-      withSgn(lhs, { (lhsSgn: Int) =>
-        withSgn(rhs, { (rhsSgn: Int) =>
+      withSgn(lhs, (lhsSgn: Int) => {
+        withSgn(rhs, (rhsSgn: Int) => {
           def k(retMag: I, sign: Int) = { sgnSet(retMag, sign); f(retMag) }
-          if (lhsSgn != rhsSgn) withAddMag(lhs, rhs, { (retMag: I) =>
+          if (lhsSgn != rhsSgn) withAddMag(lhs, rhs, (retMag: I) => {
             k(retMag, lhsSgn)
           })
-          else withMagDiff(lhs, rhs, { (retMag: I) =>
+          else withMagDiff(lhs, rhs, (retMag: I) => {
             k(retMag, sgnProd(lhsSgn, cmp(lhs, rhs)))
           })
         })
@@ -114,10 +114,10 @@ private[big] abstract class BaselessArrayInt extends Big {
     }
   }
   def withMul[U](lhs: I, rhs: I, f: I => U): U = {
-    withSgn(lhs, { (lhsSgn: Int) =>
-      withSgn(rhs, { (rhsSgn: Int) =>
-        withMulMag(lhs, rhs, { (t: I) =>
-          withNoLeadingZeroes(t, { (product: I) =>
+    withSgn(lhs, (lhsSgn: Int) => {
+      withSgn(rhs, (rhsSgn: Int) => {
+        withMulMag(lhs, rhs, (t: I) => {
+          withNoLeadingZeroes(t, (product: I) => {
             sgnSet(product, sgnProd(lhsSgn, rhsSgn))
             f(product)
           })
@@ -134,8 +134,8 @@ private[big] abstract class BaselessArrayInt extends Big {
 
   def cmp(lhs: I, rhs: I): Int = {
     var ret = 0
-    withSgn(lhs, { (lhsSgn: Int) =>
-      withSgn(rhs, { (rhsSgn0: Int) =>
+    withSgn(lhs, (lhsSgn: Int) => {
+      withSgn(rhs, (rhsSgn0: Int) => {
         val rhsSgn = if (lhs eq rhs) lhsSgn else rhsSgn0 // XXX work around mut.
         ret = if (lhsSgn == rhsSgn) lhsSgn * cmpMag(lhs, rhs)
         else cmpElem(lhsSgn, rhsSgn)
@@ -145,7 +145,7 @@ private[big] abstract class BaselessArrayInt extends Big {
   }
 
   private val hexDigitWidth = (baseLog + 3) / 4
-  private[big] def repr(n: I) = withSgn(n, { (sgn: Int) =>
+  private[big] def repr(n: I) = withSgn(n, (sgn: Int) => {
     (if (sgn < 0) "-" else "") + n.map(s"%${hexDigitWidth}X" format _).mkString
   })
 
@@ -153,7 +153,7 @@ private[big] abstract class BaselessArrayInt extends Big {
 
   private val one = {
     var ret: I = uninit()
-    fromInt(1, { (n: I) => ret = n })
+    fromInt(1, (n: I) => { ret = n })
     ret
   }
 
@@ -298,13 +298,13 @@ private[big] abstract class BaselessArrayInt extends Big {
 
     def binarySearch(lo: I, hi: I): U = {
       if (cmp(lo, hi) < 0) {
-        withAddMag(hi, one, { (hiPlus1: I) =>
-          withAddMag(lo, hiPlus1, { (twiceMid: I) =>
-            withDiv2Mag(twiceMid, { (mid: I) =>
-              withMul(mid, rhs, { (product: I) =>
+        withAddMag(hi, one, (hiPlus1: I) => {
+          withAddMag(lo, hiPlus1, (twiceMid: I) => {
+            withDiv2Mag(twiceMid, (mid: I) => {
+              withMul(mid, rhs, (product: I) => {
                 if (cmp(product, lhs) <= 0)
                   binarySearch(mid, hi)
-                else withSubMag(mid, one, { (hiNext: I) =>
+                else withSubMag(mid, one, (hiNext: I) => {
                   binarySearch(lo, hiNext)
                 })
               })
@@ -313,8 +313,8 @@ private[big] abstract class BaselessArrayInt extends Big {
         })
       } else f(lo)
     }
-    withPowBase(loLog, { (lo: I) =>
-      withPowBase(hiLog, { (hi: I) =>
+    withPowBase(loLog, (lo: I) => {
+      withPowBase(hiLog, (hi: I) => {
         binarySearch(lo, hi)
       })
     })
@@ -425,7 +425,7 @@ private[big] abstract class BaselessListInt extends Big {
 
   def cmp(lhs0: I, rhs0: I): Int = {
     var ret = 0
-    reverse(lhs0, { (lhs0: I) => reverse(rhs0, { (rhs0: I) =>
+    reverse(lhs0, (lhs0: I) => { reverse(rhs0, (rhs0: I) => {
       def rec(lhs: I, rhs: I) = {
         val moreLhs = lhs.nonEmpty
         val moreRhs = rhs.nonEmpty
--- other-lang/miniscala/src/pidigits.scala
+++ other-lang/miniscala/src/pidigits.scala
@@ -12,37 +12,37 @@ class CPS[M <: lib.big.Big](protected val module: M) {
     val (kZero, kOne, kTen) = (BigInt(0), BigInt(1), BigInt(10))
     val (kTwo, kThree) = (BigInt(2), BigInt(3))
 
-    var k = 0
-    var ns = 0
+    var k = 0;
+    var ns = 0;
 
     def rec(i: Int, nDivK: BigInt, d0: BigInt, a0: BigInt): Unit = {
       if (i < limit) {
         k = k + 1;
-        withMul(nDivK, kTwo, { (t0: BigInt) =>
-          withBigFromInt(k, { (kBig: BigInt) =>
-            withMul(nDivK, kBig, { (n: BigInt) =>
-              withBigFromInt(2*k + 1, { (k2: BigInt) =>
-                withAdd(a0, t0, { (a0t0sum: BigInt) =>
-                  withMul(a0t0sum, k2, { (a: BigInt) =>
-                    withMul(d0, k2, { (d: BigInt) =>
+        withMul(nDivK, kTwo, (t0: BigInt) => {
+          withBigFromInt(k, (kBig: BigInt) => {
+            withMul(nDivK, kBig, (n: BigInt) => {
+              withBigFromInt(2*k + 1, (k2: BigInt) => {
+                withAdd(a0, t0, (a0t0sum: BigInt) => {
+                  withMul(a0t0sum, k2, (a: BigInt) => {
+                    withMul(d0, k2, (d: BigInt) => {
                       if (a >= n) {
-                        withMul(n, kThree, { (nMul3: BigInt) =>
-                          withAdd(nMul3, a, { (three_n_plus_a: BigInt) =>
-                            withDiv(three_n_plus_a, d, { (t: BigInt) =>
+                        withMul(n, kThree, (nMul3: BigInt) => {
+                          withAdd(nMul3, a, (three_n_plus_a: BigInt) => {
+                            withDiv(three_n_plus_a, d, (t: BigInt) => {
                               // u = three_n_plus_a % d + n;
-                              withMul(t, d, { (td: BigInt) =>
-                                withSub(three_n_plus_a, td, { (u_n: BigInt) =>
-                                  withAdd(u_n, n, { (u: BigInt) =>
+                              withMul(t, d, (td: BigInt) => {
+                                withSub(three_n_plus_a, td, (u_n: BigInt) => {
+                                  withAdd(u_n, n, (u: BigInt) => {
                                     if (d > u) {
-                                      ns = ns * 10 + t.intValue
+                                      ns = ns * 10 + t.intValue;
                                       val iNext = i + 1;
                                       if (iNext % 5 == 0) {
-                                        report5(ns)
+                                        report5(ns);
                                         ns = 0
-                                      }
-                                      withSub(a, td, { (a_td: BigInt) =>
-                                        withMul(a_td, kTen, { (aNext: BigInt) =>
-                                          withMul(n, kTen, { (n10: BigInt) =>
+                                      };
+                                      withSub(a, td, (a_td: BigInt) => {
+                                        withMul(a_td, kTen, (aNext: BigInt) => {
+                                          withMul(n, kTen, (n10: BigInt) => {
                                             rec(iNext, n10, d, aNext)
                                       }) }) })
                                     } else
@@ -54,7 +54,7 @@ class CPS[M <: lib.big.Big](protected val module: M) {
               }) }) }) })
         }) }) })
       }
-    }
+    };
     rec(0, kOne, kOne, kZero)
   }
 
